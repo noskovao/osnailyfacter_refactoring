2c88414c7818ad46aa8bda00d1b54f739ae9d8ce
Alexander Noskov <anoskov@mirantis.com>
2016-03-09 17:04:43 +0300

    second commit

--- additional_scripts.sh
+++ additional_scripts.sh
@@ -1,11 +1,14 @@
 # FOR INCLUDE
-find manifests/ -name '*.pp' | xargs sed -i "s/include\ /include ::/g"
-find manifests/ -name '*.pp' | xargs sed -i "s/::::/::/g"
+find . -name '*.pp' | xargs sed -i "s/include\ /include ::/g"
+find . -name '*.pp' | xargs sed -i "s/::::/::/g"
 
 # FOR CLASSES
-find manifests/ -name '*.pp' | xargs sed -i "s/class {'/class { '/g"
-find manifests/ -name '*.pp' | xargs sed -i "s/class { '/class { '::/g"
-find manifests/ -name '*.pp' | xargs sed -i "s/class { '::::/class { '::/g"
+find . -name '*.pp' | xargs sed -i "s/class {'/class { '/g"
+find . -name '*.pp' | xargs sed -i "s/class { '/class { '::/g"
+find . -name '*.pp' | xargs sed -i "s/class { '::::/class { '::/g"
 
-find manifests/ -name '*.pp' | xargs sed -i "s/Class\['/Class\['::/g"
-find manifests/ -name '*.pp' | xargs sed -i "s/Class\['::::/Class\['::/g"
+find . -name '*.pp' | xargs sed -i "s/Class\['/Class\['::/g"
+find . -name '*.pp' | xargs sed -i "s/Class\['::::/Class\['::/g"
+
+# Replace two new lines
+find . -name '*.pp' | xargs sed -i ":a;/^$/N;/\n$/{D;ba}"
